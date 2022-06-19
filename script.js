edac5702a1cc2dcff91d960fa4cdd1c898259655
Sanzhar <sanzharburibayev@gmail.com>
2022-06-19 17:32:03 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+"use strict";
+// alert('Hello');
+
+//const result = confirm("Are u here?");
+
+//const answer = prompt("Ты кто?", "");
+//console.log(answer);
+
+// const category = 'toys';
+
+// console.log(`https://someurl.com/${category}/3`);
+
+// console.log('arr' + ' obj');
+// console.log(5 + 6);
+// let incr = 10, decr = 10;
+// // incr++;
+// // decr--;
+// console.log(++incr, --decr);
+
+let mamaMia = false,
+  mamaSia = true;
+console.log(!mamaMia && mamaSia);
